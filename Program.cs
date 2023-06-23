5d033837b831cca6248e54667548bd80e9e3f849
F R <raufi4u@gmail.com>
2023-06-23 22:21:53 +0200

    [update]

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
 ﻿using Multithreading;
+using static Multithreading.ThreadFunction;
 
 class Program
 {
     static void Main(string[] args)
     {
-        ThreadSample threadSample = new ThreadSample();
-        ThreadStartDelegateSample threadStartDelegateSample = new ThreadStartDelegateSample();
+        //ThreadSample threadSample = new ThreadSample();
+        //ThreadStartDelegateSample threadStartDelegateSample = new ThreadStartDelegateSample();
+
+        //NumberHelper numberHelper = new NumberHelper();
+        // numberHelper.ThreadFunctionSafe();
+
+        ThreadFunction threadFunction = new ThreadFunction();
+        threadFunction.ThreadFunctionSafe();
+        threadFunction.ThreadMethodCalc();
+        
+
     }
 }
