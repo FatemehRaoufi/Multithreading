f402f1ec17fe31e6e8e57f3d247af1a0585f3cef
F R <raufi4u@gmail.com>
2023-06-25 01:02:35 +0200

    [update]

--- Program.cs
+++ Program.cs
@@ -11,10 +11,25 @@ class Program
         //NumberHelper numberHelper = new NumberHelper();
         // numberHelper.ThreadFunctionSafe();
 
-        ThreadFunction threadFunction = new ThreadFunction();
-        threadFunction.ThreadFunctionSafe();
-        threadFunction.ThreadMethodCalc();
-        
+        //ThreadFunction threadFunction = new ThreadFunction();
+        //threadFunction.ThreadFunctionSafe();
+        //threadFunction.ThreadMethodCalc();
+
+        //JoinMethod joinMethod = new JoinMethod();
+
+        //joinMethod.JoinMethodSample1();
+        //joinMethod.JoinMethodSample2();
+
+        //IsAlive isAlive =   new IsAlive();
+
+        //ThreadSynchronizationSample1 threadSynchronizationSample1 = new ThreadSynchronizationSample1();
+        //ThreadSynchronizationLock2 threadSynchronizationSample2 = new ThreadSynchronizationLock2();
+
+        //ThreadSynchronizationMonitor threadSynchronizationMonitor = new ThreadSynchronizationMonitor();
+
+        //TryEnterMonitor tryEnterMonitor = new TryEnterMonitor();
+
 
     }
 }
+//https://dotnettutorials.net/lesson/multithreading-using-monitor/
